923d2d3013754903475ab1b4ac16a99da76fff92
takataka-koga <149985862+takataka-koga@users.noreply.github.com>
2023-12-10 11:38:41 +0900

    kadai_004を追加しました

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+/*
+1	HTMLドキュメントが読み込み完了した際に、「loadイベントが発生しました」とコンソールに出力されている
+2	画面スクロールした際に、「scrollイベントが発生しました」とコンソールに出力されている */
+
+  $(window).on('load', function (){
+    console.log('loadイベントが発生しました');
+  });
+
+  $(window).on('scroll', function (){
+    console.log('scrollイベントが発生しました');
+  });
